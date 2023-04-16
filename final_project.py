ed9964639c8275d413a8b7abf77bd05ee949e802
NewEra03 <128094774+NewEra03@users.noreply.github.com>
2023-04-16 17:52:56 -0400

    Create final_project.py
    Final project for INST326

---
+++ final_project.py
@@ -0,0 +1,52 @@
+"""A template for a python script deliverable for INST326.
+Names: Daniel Jeong, Irfan Mushfique, Nooran Elmostafa, Yaw Apraku
+File: final_project.py
+Assignment: Final Project
+Due Date: -------
+Challenges Encountered: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+
+class House:
+    def __int__(self, size, age, bedrooms, bathrooms, windows, location, doors, crime_rate):
+        """
+        :param size: The size of the home, in square feet
+        :param age: The age of the home, in years
+        :param bedrooms: Number of bedrooms in the home
+        :param bathrooms: Number of bathrooms in the home
+        :param windows: Number of windows installed throughout the house
+        :param location: Where the home is located in
+        :param doors: Number of doors throughout the house
+        :param crime_rate: The crime rate within the area
+        An optional parameter for this is garage (this will be in boolean) 
+        :return: New house object containing the above parameters
+        """
+        pass
+    
+
+def size_value(size):
+    pass
+
+def age_value(age):
+    pass
+
+def bedrooms_bathrooms_value(bedroom, bathroom):
+    pass
+
+def doors_windows_value(doors, windows):
+    pass
+
+def location_value(location):
+    pass
+
+def safety_value(crime_rate):
+    pass
+
+def arguments_checker(argument):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    pass
